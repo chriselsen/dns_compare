5deeffb0a0faed65efaf1ffe6e26add8326bbe70
Christian Elsen <chriselsen@users.noreply.github.com>
2023-06-20 17:44:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup
 
 setup(
     name='dns_compare',
-    version='0.0.3',
-    author='Joe Miller',
-    author_email='joeym@joeym.net',
+    version='0.0.4',
+    author='Christian Elsen',
     scripts=['dns_compare'],
-    url='https://github.com/joemiller/dns_compare',
+    url='https://github.com/chriselsen/dns_compare',
     license='LICENSE',
     description="Test DNS servers",
     long_description=open('README.markdown').read()
